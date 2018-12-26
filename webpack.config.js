f49a5963623c5fbb55292eb377cf4590509fc571
Vincent <v.rell_home@outlook.com>
2018-12-26 17:04:55 +0100

    installed webpack and plugins

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        main: "./app/assets/scripts/main.js"
+    },
+    output:{
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\html$/,
+                use: [{loader: "html-loader", options: {minimize: false}}]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "./images/",
+                            publicPath: "./images/"
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./app/index.html",
+            filename: "./index.html"
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
