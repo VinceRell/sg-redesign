6ba2d2bb3ca42a600c947c6680d946a68db71532
Vincent <v.rell_home@outlook.com>
2019-01-04 03:14:17 +0100

    added footer and stylng to page

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -6,6 +6,4 @@ import "../images/logo.png";
 import "../images/hero--small.jpg";
 import "../images/hero--medium.jpg";
 import "../images/hero--large.jpg";
-import "../images/001-shield.png";
-import "../images/002-idea.png";
-import "../images/003-agreement.png";
+import "../images/erkend.jpg";
